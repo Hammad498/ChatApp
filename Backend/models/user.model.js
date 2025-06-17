5e2df4f3c18a3e2a41277ea162c2a07f31dd38fc
Hammad498 <hammadabrar498@gmail.com>
2025-06-17 17:19:53 +0500

    userModelSetup

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const userSchema=new Schema({
     username:{
@@ -30,6 +32,25 @@ const userSchema=new Schema({
     }
 
 })
+
+//hash password before saving
+userSchema.pre("save",async function(next){
+    if(this.isModified("password")){
+        this.password=await bcrypt.hash(this.password,10);
+    }
+    next();
+})
+
+//method to generate JWT token
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: '10h' });
+    return token;
+};
+
+//method to compare password
+userSchema.methods.comparePassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
 
 const User=mongoose.model("Users",userSchema);
 export default User;
