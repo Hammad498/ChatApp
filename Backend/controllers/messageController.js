a59ae2ccc30bc372c18c06da851bfbdcee04bcf0
Hammad498 <hammadabrar498@gmail.com>
2025-06-17 17:55:46 +0500

    createMsg

--- Backend/controllers/messageController.js
+++ Backend/controllers/messageController.js
@@ -11,6 +11,7 @@ export const getAllMsg=async(req,res)=>{
             users: {
                 $all: [from, to]
             }
+            // $or:[{sender : from, reciever : to,},{sender : to, reciever : from,}]
         }).sort({updatedAt: 1});
         
         const projectedMessages = messages.map(msg => {
@@ -27,4 +28,25 @@ export const getAllMsg=async(req,res)=>{
 }
 
 
-export const addMsg=async(req,res)=>{}
+export const addMsg=async(req,res)=>{
+    const {from,to,message}=req.body;
+    if(!from || !to || !message){
+        return res.status(400).json({message: "From, To and Message fields are required"});
+    }
+    try {
+        const data = await Message.create({
+            message: { text: message },
+            // users: [from, to],
+            sender: from,
+            reciever: to
+        });
+        
+        if(data){
+            return res.status(200).json({message: "Message added successfully"});
+        } else {
+            return res.status(400).json({message: "Failed to add message"});
+        }
+    } catch (error) {
+        res.status(500).json({message: "Error adding message", error: error.message});
+    }
+}
